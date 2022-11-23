f78eda2ea7806530f3d75fd0106f6277b00ae5df
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-11-23 23:53:46 +0545

    popup idea

--- src/components/portfolio/Card.jsx
+++ src/components/portfolio/Card.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Card = (props) => {
+  const [modal, setModal] = useState(false);
+  const toggleModel = () => {
+    setModal(!modal);
+  };
   return (
     <>
       <div className="box btn_shadow">
         <div className="img">
-          <img src={props.image} alt="" />
+          <img src={props.image} alt="" onClick={toggleModel} />
         </div>
         <div className="category d_flex">
-          <span>{props.category}</span>
+          <span onClick={toggleModel}>{props.category}</span>
           <label>
             <i className="far fa-heart"></i>
             {props.totalLike}
@@ -16,8 +20,21 @@ const Card = (props) => {
         </div>
         <div className="title">
           <h2>{props.title}</h2>
+          <a href="" className="arrow" onClick={toggleModel}>
+            <i className="fas fa-arrow-right"></i>
+          </a>
         </div>
       </div>
+      {modal && (
+        <div className="modal">
+          <div onClick={toggleModel} className="overlay"></div>
+          <div className="modal-content d_flex">
+            <div className="modal-img left">
+              <img src={props.image} alt="" />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
