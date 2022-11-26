eefee1af4a8710329533b5c12f859b23dae2a7cc
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-11-26 21:34:06 +0545

    testional

--- src/components/Testimonial/Slide.jsx
+++ src/components/Testimonial/Slide.jsx
@@ -1,24 +1,52 @@
 import React from "react";
-import TestimonialAPI from "../Testimonial/TestimonialAPI";
+import TestimonialApi from "../Testimonial/TestimonialAPI";
 
-const Slide = ({ id, image, design, name, officer, post, date, desc }) => {
+const Slide = ({
+  id,
+  image,
+  design,
+  name,
+  offcer,
+  post,
+  date,
+  desc,
+  valueIndex,
+  index,
+}) => {
+  let position = "nextSlide";
+  if (valueIndex === index) {
+    position = "activeSlide";
+  }
+  if (
+    valueIndex === index - 1 ||
+    (index === 0 && valueIndex === TestimonialApi.length - 1)
+  ) {
+    position = "lastSlide";
+  }
   return (
     <>
-      <article className="d_flex">
+      <article className={`d_flex ${position}`} key={id}>
         <div className="left box_shodow">
-          <div className="img">{/* <img src={image} alt="" /> */}</div>
+          <div className="img">
+            <img
+              src="https://rainbowit.net/html/inbio/assets/images/testimonial/final-home--4th.png"
+              alt=""
+            />
+          </div>
           <div className="details mtop">
             <span className="primary_color">{design}</span>
             <h2>{name}</h2>
-            <label>{officer}</label>
+            <label>{offcer}</label>
           </div>
         </div>
+
         <div className="right">
           <div className="icon">
             <div className="quote">
-              <i className="fal fa-quote-right"></i>
+              <i class="fas fa-quote-right"></i>
             </div>
           </div>
+
           <div className="content box_shodow mtop">
             <h1>{post}</h1>
             <h3>{date}</h3>
